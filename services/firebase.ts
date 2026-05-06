4ff4f13f9c42b4dbe903caaa11dd2d3217691578
Pedro Miguel Beloso Berdun <Pbelosoberdun@gmail.com>
2026-05-06 20:50:40 -0300

    feat: agregar pantallas de login y registro con validación y Firebase Auth (#1)

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,19 +1,25 @@
+/** 
+BACKEND - Configuración de Firebase
+Este archivo solo lo importan los servicios (ej: auth.service.ts).
+El FRONTEND nunca importa este archivo directamente.
+*/
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  // TODO: Add your Firebase config here
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyAXQngNZX1gjb_03pWCsSsyFEubrdasVZk",
+  authDomain: "tp3---gestor-de-gastos.firebaseapp.com",
+  projectId: "tp3---gestor-de-gastos",
+  storageBucket: "tp3---gestor-de-gastos.firebasestorage.app",
+  messagingSenderId: "52896218835",
+  appId: "1:52896218835:web:4cc762c026209cf82d9e26"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
--- store/authStore.ts
+++ store/authStore.ts
@@ -1,3 +1,10 @@
+
+ /*FRONTEND - Estado global de autenticación (Zustand)
+ Maneja el estado del usuario logueado en toda la app.
+ El BACKEND (auth.service) actualiza este estado cuando cambia la sesión.
+ El FRONTEND (pantallas) lee este estado para saber si hay usuario.*/
+
+
 import { create } from 'zustand';
 import { User } from 'firebase/auth';
 
